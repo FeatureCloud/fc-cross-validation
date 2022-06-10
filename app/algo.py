06d20e2bd8595ee6541d296306e273121925cd28
Julian Späth <spaethju@posteo.de>
2022-06-10 11:54:11 -0400

    Redo repeated cv

--- app/algo.py
+++ app/algo.py
@@ -1,6 +1,6 @@
 import os
 
-from sklearn.model_selection import RepeatedStratifiedKFold, RepeatedKFold
+from sklearn.model_selection import StratifiedKFold, KFold
 import pandas as pd
 
 
@@ -8,11 +8,11 @@ def read_data(path, sep):
     return pd.read_csv(path, sep=sep)
 
 
-def create_splits(data, label, n_splits, stratify, n_repeats, random_state, output_dir):
+def create_splits(data, label, n_splits, stratify, shuffle, random_state, output_dir):
     os.makedirs(output_dir, exist_ok=True)
     if stratify and (label is not None):
         print(f'Use stratified kfold cv for label column {label}', flush=True)
-        cv = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=random_state)
+        cv = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
         y = data.loc[:, label]
         for k, (train_indices, test_indices) in enumerate(cv.split(data, y)):
             path = f'{output_dir}/split_{str(k + 1)}'
@@ -21,7 +21,7 @@ def create_splits(data, label, n_splits, stratify, n_repeats, random_state, outp
             pd.DataFrame(data.iloc[test_indices], columns=data.columns).to_csv(f'{path}/test.csv', index=False)
     else:
         print(f'Use kfold cv', flush=True)
-        cv = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=random_state)
+        cv = KFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
         for k, (train_indices, test_indices) in enumerate(cv.split(data)):
             path = f'{output_dir}/split_{str(k + 1)}'
             os.makedirs(path, exist_ok=True)
